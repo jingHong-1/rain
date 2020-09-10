ecc8f4e3792f3e7c6c4b15bd70aae803a52fb138
Rain999 <1163799833@qq.comgit>
2020-09-10 16:21:49 +0800

    init repo

---
+++ src/test/java/com/example/rain/RainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
