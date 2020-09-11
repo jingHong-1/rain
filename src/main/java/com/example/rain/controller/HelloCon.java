c5565546e887bf1b3b8fdf7da895462161f5d5e6
Rain999 <1163799833@qq.comgit>
2020-09-11 09:39:16 +0800

    add bootstrap

--- src/main/java/com/example/rain/controller/HelloCon.java
+++ src/main/java/com/example/rain/controller/HelloCon.java
@@ -5,16 +5,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class HelloCon {
     @GetMapping("/hello")
-
    public String Hello(@RequestParam(name="name") String name,Model model){
 
         model.addAttribute("name",name);
        return "hello";
     }
+
+
+    //ModelAndView 模式
+    @GetMapping("/hello2")
+    public ModelAndView hello2(@RequestParam(name = "name") String name){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name",name);
+        mv.setViewName("hello2");
+        return mv;
+
+    }
+
 }
